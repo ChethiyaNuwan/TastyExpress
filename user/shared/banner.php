ec6390540f59cfe217084ae8c239a48c89779783
ChethiyaNuwan <115537042+ChethiyaNuwan@users.noreply.github.com>
2023-07-25 02:40:05 +0530

    added headline over banner

--- user/shared/banner.php
+++ user/shared/banner.php
@@ -3,4 +3,11 @@
     <img src="../images/banner2.jpg" alt="banner 2">
     <img src="../images/banner3.jpg" alt="banner 3">
     <img src="../images/banner4.jpg" alt="banner 4">
+
+</div>
+<div class="banner-text">
+    <h1>It's Tasty - It's Express</h1>
+    <h3>Order delicious foods made by our finest chefs online and get it delivered to your doorstep fast</h3>
+    <br><br>
+    <a href="#">Order Now</a>
 </div>
