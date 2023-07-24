3ff9e45a9097149921a8308b264f069191834e25
ChethiyaNuwan <115537042+ChethiyaNuwan@users.noreply.github.com>
2023-07-24 22:07:20 +0530

    git init

---
+++ user/shared/banner.php
@@ -0,0 +1,6 @@
+<div class="banner">
+    <img src="../../images/banner1.jpg" alt="banner 1">
+    <img src="../../images/banner2.jpg" alt="banner 2">
+    <img src="../../images/banner3.jpg" alt="banner 3">
+    <img src="../../images/banner4.jpg" alt="banner 4">
+</div>
---
+++ user/shared/header.php
@@ -0,0 +1,11 @@
+<nav class="navbar">
+    <span class="navbar-brand">TastyExpress</span>
+    <span class="nav-list">
+            <a href="#">Home</a>
+            <a href="#">About Us</a>
+            <a href="#">Categories</a>
+            <a href="#">Foods</a>
+            <a href="#">Contact</a>
+            <a href="#">Login</a>
+        </span>
+</nav>
