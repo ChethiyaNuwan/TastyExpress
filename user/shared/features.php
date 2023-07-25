b811b6566a0b5daedc5680da92b5dcc82c69b506
ChethiyaNuwan <115537042+ChethiyaNuwan@users.noreply.github.com>
2023-07-26 01:47:41 +0530

    created about us, features and footer

---
+++ user/shared/features.php
@@ -0,0 +1,14 @@
+<div class="features">
+    <div>
+        <i>local_dining</i>
+        <h2>Variety of Dishes</h2>
+    </div>
+    <div>
+        <i>local_shipping</i>
+        <h2>Fast Delivery</h2>
+    </div>
+    <div>
+        <i>mood</i>
+        <h2>Excellent Quality</h2>
+    </div>
+</div>
---
+++ user/shared/footer.php
@@ -0,0 +1,13 @@
+<footer>
+    <div>
+        <h1>TastyFoods Restaurants</h1>
+        <p>No. 123,</p>
+        <p>Yatinuwara Veediya,</p>
+        <p>Kandy</p>
+    </div>
+    <div>
+        <h2>Find Us On Social</h2>
+        <p><a>Facebook</a></p>
+        <p><a>Instagram</a></p>
+    </div>
+</footer>
