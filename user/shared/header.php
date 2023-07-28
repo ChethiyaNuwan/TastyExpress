d9e8a3c356ade5706c802db4844a285167f27044
ChethiyaNuwan <115537042+ChethiyaNuwan@users.noreply.github.com>
2023-07-28 20:42:43 +0530

    added links

--- user/shared/header.php
+++ user/shared/header.php
@@ -1,8 +1,8 @@
 <nav class="navbar">
     <span class="navbar-brand">TastyExpress</span>
     <span class="nav-list">
-            <a href="#">Home</a>
-            <a href="#">Categories</a>
+            <a href="index.php">Home</a>
+            <a href="categories.php">Categories</a>
             <a href="#">Foods</a>
             <a href="#">Contact</a>
             <a href="#">Login</a>
