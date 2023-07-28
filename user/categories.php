1b7009332c959a342adbf536ed76e3bd45929d08
ChethiyaNuwan <115537042+ChethiyaNuwan@users.noreply.github.com>
2023-07-28 20:43:40 +0530

    created categories php

---
+++ user/categories.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>TastyExpress - Wide Range Of Categories To Choose From</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <link rel="stylesheet" href="../stylesheets/styles.css">
+</head>
+<body>
+
+<?php
+include ('shared/header.php');
+?>
+
+</body>
+</html>
