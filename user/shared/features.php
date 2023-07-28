6dce6976b5ad07b808236d17b0b4f300c5b2f02d
ChethiyaNuwan <115537042+ChethiyaNuwan@users.noreply.github.com>
2023-07-28 20:41:56 +0530

    added images

--- user/shared/features.php
+++ user/shared/features.php
@@ -1,14 +1,14 @@
 <div class="features">
     <div>
-        <i>local_dining</i>
+        <img src="../images/variety_foods.svg" alt="variety foods">
         <h2>Variety of Dishes</h2>
     </div>
     <div>
-        <i>local_shipping</i>
+        <img src="../images/fast_delivery.svg" alt="fast delivery">
         <h2>Fast Delivery</h2>
     </div>
     <div>
-        <i>mood</i>
+        <img src="../images/mood_satisfied.svg" alt="mood satisfied">
         <h2>Excellent Quality</h2>
     </div>
 </div>
