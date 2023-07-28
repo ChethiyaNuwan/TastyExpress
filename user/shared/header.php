7f5ad0103bf4cee725bd1742e691bdf26654ba30
ChethiyaNuwan <115537042+ChethiyaNuwan@users.noreply.github.com>
2023-07-28 20:46:43 +0530

    added link to brand name and its css

--- user/shared/header.php
+++ user/shared/header.php
@@ -1,5 +1,5 @@
 <nav class="navbar">
-    <span class="navbar-brand">TastyExpress</span>
+    <span class="navbar-brand"><a href="#">TastyExpress</a></span>
     <span class="nav-list">
             <a href="index.php">Home</a>
             <a href="categories.php">Categories</a>
