b98f4bbd7a7beb946de2b80ef6b79cd16121b0be
ChethiyaNuwan <115537042+ChethiyaNuwan@users.noreply.github.com>
2023-08-03 13:56:58 +0530

    css link updated

--- user/register.php
+++ user/register.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <title>TastyExpress - Register</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <link rel="stylesheet" href="../stylesheets/styles.css">
+    <link rel="stylesheet" href="../stylesheets/styles.css?1">
 </head>
 <body>
 
