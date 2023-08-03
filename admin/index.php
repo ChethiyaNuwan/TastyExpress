50e6b4d093ca0c7fee68367d645bb28089068995
yukthi hettiarachchi <101061736+yukthi2000@users.noreply.github.com>
2023-08-03 23:49:27 +0530

    admin loggin

--- admin/index.php
+++ admin/index.php
@@ -22,73 +22,19 @@ if(isset($_SESSION['msg']))
 </head>
 <body style="font-family: Verdana, sans-serif;">
 
+<body>
 
-    <div class="login-page section">
-        <div class="center-align">
-            <div class="row">
 
-            <div class="col s12">
-
-                <div class="container">
-                    <div class="container">
-                        <div class="container">
-                            <div class="card horizontal hoverable">
-
-                                <div class="card-stacked">
-                                    <form class="card-content" action="../backend/admin/login-admin.php" method="post">
+   <form class="card login-card" action="../backend/admin/login-admin.php" method="post">
                                         <h4 class="header">Admin Login</h4>
+    <label for="email">Email</label>
+    <input type="email" id="email" name="email" required>
+    <label for="password">Password</label>
+    <input type="password" id="password" name="password" required>
+    <button class="link-button" type="submit">Login</button>
 
-                                        <?php
+</form>
 
-                                            if(!empty($msg_error)){
-                                                echo '<div class="row error-msg">
-                                                            <div class="col">
-                                                                <b>'.$msg_error.'</b>
-                                                            </div>
-                                                        </div>';
-
-                                            }
-                                        ?>
-
-                                        <div class="row">
-                                            <div class="input-field col s12">
-                                                <span for="email"><b>Email</b></span>
-                                                <input name="email" id="email" type="email" class="validate">
-                                            </div>
-                                        </div>
-
-                                        <div class="row">
-                                            <div class="input-field col s12">
-                                            <span for="email"><b>Password</b></span>
-                                                <input id="password" name="password" type="password" class="validate">
-                                            </div>
-                                        </div>
-
-                                        <div class="row">
-                                            <div class="col s12">
-                                                <button type="submit"  class="waves-effect waves-light btn"><b>Log In</b></button>
-                                            </div>
-                                        </div>
-
-                                    </form>
-                                </div>
-                            </div>
-
-                        </div>
-                    </div>
-                </div>
-
-
-            </div>
-
-
-
-
-
-            </div>
-        </div>
-    </div>
-
-
+</body>
 </body>
 </html>
